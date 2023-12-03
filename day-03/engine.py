c9496857de515e97d765a1124babb0cc4e65a8a4
ab-n3xt <46637527+ab-n3xt@users.noreply.github.com>
2023-12-03 15:57:54 -0500

    Part two completed

--- day-03/engine.py
+++ day-03/engine.py
@@ -4,6 +4,9 @@ def find_adjacent_numbers(lines: str, line_index: int, char_index: int):
 
     total = 0
 
+    part_number_count = 0
+    gear_ratio = 1
+
     if above_line >= 0:
         # Check above
         if lines[above_line][char_index].isdigit():
@@ -13,6 +16,9 @@ def find_adjacent_numbers(lines: str, line_index: int, char_index: int):
             while right_index+1 < len(lines) and lines[above_line][right_index+1].isdigit():
                 right_index += 1
             total += int(lines[above_line][left_index:right_index+1])
+            
+            part_number_count += 1
+            gear_ratio *= int(lines[above_line][left_index:right_index+1])
         else:
             # Check top-left
             if left_side >= 0 and lines[above_line][left_side].isdigit():
@@ -20,12 +26,18 @@ def find_adjacent_numbers(lines: str, line_index: int, char_index: int):
                 while left_index-1 >= 0 and lines[above_line][left_index-1].isdigit():
                     left_index -= 1
                 total += int(lines[above_line][left_index:right_index+1])
+
+                part_number_count += 1
+                gear_ratio *= int(lines[above_line][left_index:right_index+1])
             # Check top-right
             if right_side < len(lines) and lines[above_line][right_side].isdigit():
                 left_index, right_index = right_side, right_side
                 while right_index+1 < len(lines) and lines[above_line][right_index+1].isdigit():
                     right_index += 1
                 total += int(lines[above_line][left_index:right_index+1])
+
+                part_number_count += 1
+                gear_ratio *= int(lines[above_line][left_index:right_index+1])
 
     if below_line < len(lines):
         # Check below
@@ -36,6 +48,9 @@ def find_adjacent_numbers(lines: str, line_index: int, char_index: int):
             while right_index+1 < len(lines) and lines[below_line][right_index+1].isdigit():
                 right_index += 1
             total += int(lines[below_line][left_index:right_index+1])
+            
+            part_number_count += 1
+            gear_ratio *= int(lines[below_line][left_index:right_index+1])
         else:
             # Check below-left
             if left_side >= 0 and lines[below_line][left_side].isdigit():
@@ -43,26 +58,41 @@ def find_adjacent_numbers(lines: str, line_index: int, char_index: int):
                 while left_index-1 >= 0 and lines[below_line][left_index-1].isdigit():
                     left_index -= 1
                 total += int(lines[below_line][left_index:right_index+1])
+
+                part_number_count += 1
+                gear_ratio *= int(lines[below_line][left_index:right_index+1])
             # Check below-right
             if right_side < len(lines) and lines[below_line][right_side].isdigit():
                 left_index, right_index = right_side, right_side
                 while right_index+1 < len(lines) and lines[below_line][right_index+1].isdigit():
                     right_index += 1
                 total += int(lines[below_line][left_index:right_index+1])
+
+                part_number_count += 1
+                gear_ratio *= int(lines[below_line][left_index:right_index+1])
     # Check left
     if left_side >= 0 and lines[line_index][left_side].isdigit():
         left_index, right_index = left_side, left_side
         while left_index-1 >= 0 and lines[line_index][left_index-1].isdigit():
             left_index -= 1
         total += int(lines[line_index][left_index:right_index+1])
+
+        part_number_count += 1
+        gear_ratio *= int(lines[line_index][left_index:right_index+1])
     # Check right
     if right_side < len(lines) and lines[line_index][right_side].isdigit():
         left_index, right_index = right_side, right_side
         while right_index+1 < len(lines) and lines[line_index][right_index+1].isdigit():
             right_index += 1
         total += int(lines[line_index][left_index:right_index+1])
+        
+        part_number_count += 1
+        gear_ratio *= int(lines[line_index][left_index:right_index+1])
     
-    return total
+    if part_number_count == 2:
+        return gear_ratio
+    else:
+        return 0
 
 
 total = 0
@@ -71,7 +101,7 @@ with open("C:\\Users\\Aidan\\Projects\\Advent-of-Code-2023\\day-03\\input.txt") 
     for line_index, line in enumerate(lines):
         line = line.strip()
         for char_index, char in enumerate(line):
-            if char != '.' and not char.isdigit():
+            if char == '*':
                 total += find_adjacent_numbers(lines, line_index, char_index)
 
 print(total)
