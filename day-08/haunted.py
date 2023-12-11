e7e1f14088f943750948646443896d5b4c034c4a
Aidan B <aidan.buchanan@gmail.com>
2023-12-11 15:31:20 -0500

    Completed part two

--- day-08/haunted.py
+++ day-08/haunted.py
@@ -1,9 +1,9 @@
-import re
+import re, math
 
 pattern = re.compile("(\w+) = \((\w+), (\w+)\)")
 
 nodes = {}
-current_node = "AAA"
+current_nodes = []
 steps = 0
 with open("input.txt") as fr:
     lines = fr.readlines()
@@ -13,28 +13,41 @@ with open("input.txt") as fr:
     
     # Process all the nodes
     for node in lines[2:]:
-        is_match = pattern.match(node)
-        if is_match:
+        if (is_match := pattern.match(node)) is not None:
             key, left_node, right_node = is_match.groups()
+            if key[2] == "A":
+                current_nodes.append(key)
             nodes[key] = (left_node, right_node)
 
-    # Loop every node until ZZZ is found
-    index = 0
-    while True:
-        instruction = instructions[index]    
-
-        if instruction == "L":
-            current_node = nodes[current_node][0]
-        elif instruction == "R":
-            current_node = nodes[current_node][1]
+    # I figured out that the step distance between finding "Z"
+    # Was the same every time. So I find all the differences,
+    # then take the LCM of the differences
+    found_z_at_step = []
+    difference_list = []
+    for i, node in enumerate(current_nodes):
+        index = 0
+        step = 0
         
-        steps += 1
+        # Calculate the number of steps needed to reach
+        # the first "Z" node and the second "Z" node
+        while len(found_z_at_step) != 2:
+            instruction = instructions[index]
+            index += 1
+            if index == len(instructions):
+                index = 0
+            match instruction:
+                case "L":
+                    node = nodes[node][0]
+                case "R":
+                    node = nodes[node][1]
+            
+            step += 1
+            if node[2] == "Z":
+                found_z_at_step.append(step)
 
-        if current_node == "ZZZ":
-            break
-        
+        e1, e2 = found_z_at_step[0], found_z_at_step[1]
+        found_z_at_step.clear()
+        difference_list.append(e2-e1)
         index += 1
-        if index == len(instructions):
-            index = 0
-    
-    print(steps)
+
+    print(math.lcm(*difference_list))
